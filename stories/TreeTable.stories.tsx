08352940170b1555eba3f31ce4cc695224301e19
Michaël <michael@mopdebeeck.be>
2022-04-27 22:39:00 +0200

    storybook TreeTable: realData top level

--- stories/TreeTable.stories.tsx
+++ stories/TreeTable.stories.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 
 import "../public/themes/nova/theme.css";
 import "../styles/primereact.css";
 import "primeicons/primeicons.css";
 
-import '../components/lib/treetable/TreeTable.css';
-import { TreeTable } from '../components/lib/treetable/TreeTable';
-import TreeNode from '../components/lib/treenode/TreeNode';
-import { Column } from '../components/lib/column/Column';
+import "../components/lib/treetable/TreeTable.css";
+import { TreeTable } from "../components/lib/treetable/TreeTable";
+import TreeNode from "../components/lib/treenode/TreeNode";
+import { Column } from "../components/lib/column/Column";
 import dbDataRockets1 from "./dbdata-rockets-1.json";
 import dbDataRaw from "./dbdata-raw.js";
 import { EJSON } from "bson";
@@ -15,46 +15,43 @@ import { EJSON } from "bson";
 const nodes: TreeNode[] = dbDataRockets1;
 
 export default {
-    title: 'PrimeReact/TreeTable',
-    component: TreeTable,
-    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-    argTypes: {},
+  title: "PrimeReact/TreeTable",
+  component: TreeTable,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {},
 };
 
 const Template = (args) => {
-    return (<TreeTable mongoDbData={args.mongoDbData} value={args.value} onKeyDown={args.onKeyDown}>
-        <Column field="key" header="Key" expander />
-        <Column field="value" header="Value" />
-        <Column field="type" header="Type" />
-    </TreeTable>);
-}
+  return (
+    <TreeTable mongoDbData={args.mongoDbData} value={args.value} onKeyDown={args.onKeyDown}>
+      <Column field="key" header="Key" expander />
+      <Column field="value" header="Value" />
+      <Column field="type" header="Type" />
+    </TreeTable>
+  );
+};
 
 export const BSON = Template.bind({});
 
 BSON.args = {
-    mongoDbData: EJSON.serialize(dbDataRaw),
-    onKeyDown: (
-        event: React.KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>,
-        node: TreeNode
-    ) => {
-        if (event.metaKey && event.key==="c") {
-            navigator.clipboard.writeText(JSON.stringify(node.data.realData, null, 2));
-            console.log('Copied value to clipboard:', node.data.realData);
-        }
+  mongoDbData: EJSON.serialize(dbDataRaw),
+  onKeyDown: (event: React.KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>, node: TreeNode) => {
+    if (event.metaKey && event.key === "c") {
+      console.log("====== ", node.data);
+      navigator.clipboard.writeText(JSON.stringify(node.data.realData, null, 2));
+      console.log("Copied value to clipboard:", node.data.realData);
     }
-}
+  },
+};
 export const JSONData = Template.bind({});
 
 JSONData.args = {
-    value: nodes,
-    mongoDbData: undefined,
-    onKeyDown: (
-        event: React.KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>,
-        node: TreeNode
-    ) => {
-        if (event.metaKey && event.key==="c") {
-            navigator.clipboard.writeText(JSON.stringify(node.data.realData, null, 2));
-            console.log('Copied value to clipboard:', node.data.value);
-        }
+  value: nodes,
+  mongoDbData: undefined,
+  onKeyDown: (event: React.KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>, node: TreeNode) => {
+    if (event.metaKey && event.key === "c") {
+      navigator.clipboard.writeText(JSON.stringify(node.data.realData, null, 2));
+      console.log("Copied value to clipboard:", node.data.value);
     }
-}
+  },
+};
--- stories/dbdata-raw.js
+++ stories/dbdata-raw.js
@@ -1,47 +1,48 @@
 import { ObjectId } from "bson";
 
-const bson = [{
-    "_id": new ObjectId("61f2e0fe799c513668bcfeb3"),
-    "now": new Date(),
-    "name": "Falcon",
-    "created": new Date("2022-01-27T19:15:08.199+0100"),
-    "launched": true,
-    "someArray": [
-        -1.11,
-        {
-            "project": new ObjectId("6202e8163cd35d5398f8aa7c")
-        },
-        3.0
+const bson = [
+  {
+    _id: new ObjectId("61f2e0fe799c513668bcfeb3"),
+    now: new Date(),
+    name: "Falcon",
+    created: new Date("2022-01-27T19:15:08.199+0100"),
+    launched: true,
+    someArray: [
+      -1.11,
+      {
+        project: new ObjectId("6202e8163cd35d5398f8aa7c"),
+      },
+      3.0,
     ],
-    "someNullField": null,
-    "someObjectField": {
-        "str": "sdif",
-        "bool": false,
-        "arr": [],
-        "date": new Date("2022-02-07T10:35:59.073+0100")
+    someNullField: null,
+    someObjectField: {
+      str: "sdif",
+      bool: false,
+      arr: [],
+      date: new Date("2022-02-07T10:35:59.073+0100"),
     },
-    "double": 3.14
-},
-    {
-        "_id": new ObjectId("6263cde66482bc292e601fa3"),
-        "name": "Apollo",
-        "created": new Date("2022-02-01T00:00:08.199+0100"),
-        "launched": false,
-        "someArray": [
-            -1.11,
-            {
-                "project": new ObjectId("6202e8163cd35d5398f8aa7c")
-            },
-            3.0
-        ],
-        "someNullField": null,
-        "someObjectField": {
-            "str": "sdif",
-            "bool": false,
-            "arr": [],
-            "date": new Date("2022-02-07T10:35:59.073+0100")
-        },
-        "double": 3.14
-    }
+    double: 3.14,
+  },
+  {
+    _id: new ObjectId("6263cde66482bc292e601fa3"),
+    name: "Apollo",
+    created: new Date("2022-02-01T00:00:08.199+0100"),
+    launched: false,
+    someArray: [
+      -1.11,
+      {
+        project: new ObjectId("6202e8163cd35d5398f8aa7c"),
+      },
+      3.0,
+    ],
+    someNullField: null,
+    someObjectField: {
+      str: "sdif",
+      bool: false,
+      arr: [],
+      date: new Date("2022-02-07T10:35:59.073+0100"),
+    },
+    double: 3.14,
+  },
 ];
-export default bson
+export default bson;
